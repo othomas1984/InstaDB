84354be1f2ad86c5688540334ae7de97e89daa8b
Owen Thomas <owen@owenhthomas.com>
2018-08-18 15:19:24 -0700

    Add basic Image model

---
+++ InstaDB/Models/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  InstaDB
+//
+//  Created by Owen Thomas on 8/18/18.
+//  Copyright © 2018 SwiftCoders. All rights reserved.
+//
+
+import Foundation
+
+struct Image {
+  let name: String
+  let path: String?
+}
