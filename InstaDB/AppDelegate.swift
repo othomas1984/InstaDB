ae83a430173d39a7aae64f6127f4c9164ef36c36
Owen Thomas <owen@owenhthomas.com>
2018-08-22 14:55:42 -0700

    Refactor app delegate to remove SwiftyDropbox

--- InstaDB/AppDelegate.swift
+++ InstaDB/AppDelegate.swift
@@ -6,41 +6,24 @@
 //  Copyright © 2018 SwiftCoders. All rights reserved.
 //
 
-// TODO: Remove SwiftyDropbox by setting up Dropbox in FileService on first use
-import SwiftyDropbox
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
   var window: UIWindow?
 
   func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    ) -> Bool {
     window?.backgroundColor = .white
-    setupDropbox()
+    FileService.setup()
     return true
   }
 
   // swiftlint:disable:next identifier_name
-  func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
-    handleDropboxCallback(url)
+  func application(_ app: UIApplication, open url: URL,
+                   options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
+    FileService.handleDropboxCallback(url)
     return true
-  }
-
-  private func setupDropbox() {
-    DropboxClientsManager.setupWithAppKey("kv804kipmgbpxnx")
-  }
-  
-  private func handleDropboxCallback(_ url: URL) {
-    guard let result = DropboxClientsManager.handleRedirectURL(url) else { return }
-    switch result {
-    case .success:
-      NotificationCenter.default.post(name: Notification.Name.dropboxAuthSuccess, object: nil)
-    case .cancel:
-      NotificationCenter.default.post(name: Notification.Name.dropboxAuthCancel, object: nil)
-    case let .error(_, description):
-      NotificationCenter.default.post(name: Notification.Name.dropboxAuthError, object: nil, userInfo: ["description": description])
-    }
   }
 }
