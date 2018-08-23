9a8b70dbe1e596265b24220789d4a4a88b7b2fc1
Owen Thomas <owen@owenhthomas.com>
2018-08-22 19:10:01 -0700

    Rework error messages

--- InstaDB/Extensions/CallErrorExtension.swift
+++ InstaDB/Extensions/CallErrorExtension.swift
@@ -11,27 +11,28 @@ import SwiftyDropbox
 extension CallError {
   var errorDescription: String {
     switch self as CallError {
-    case let .internalServerError(code, message, requestId):
-      return "InternalServerError[\(requestId ?? "Unknown")]: \(code): \(message ?? "Unknown")"
-    case let .badInputError(message, requestId):
-      return "BadInputError[\(requestId ?? "Unknown")]: \(message ?? "Unknown")"
-    case let .authError(authError, userMessage, errorSummary, requestId):
-      return "AuthError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(authError)"
-    case let .accessError(accessError, userMessage, errorSummary, requestId):
-      return "AccessError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(accessError)"
-    case let .rateLimitError(rateLimitError, userMessage, errorSummary, requestId):
-      return "RateLimitError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(rateLimitError)"
-    case let .httpError(code, message, requestId):
-      return "HTTPError[\(requestId ?? "Unknown")]: \(code ?? 0): \(message ?? "Unknown")"
+    case let .internalServerError(code, message, _):
+      return "InternalServerError: \(code): \(message ?? "Unknown")"
+    case let .badInputError(message, _):
+      return "BadInputError: \(message ?? "Unknown")"
+    case let .authError(authError, userMessage, errorSummary, _):
+      return "AuthError: \(userMessage ?? "") \(errorSummary ?? "") \(authError)"
+    case let .accessError(accessError, userMessage, errorSummary, _):
+      return "AccessError: \(userMessage ?? "") \(errorSummary ?? "") \(accessError)"
+    case let .rateLimitError(rateLimitError, userMessage, errorSummary, _):
+      return "RateLimitError: \(userMessage ?? "") \(errorSummary ?? "") \(rateLimitError)"
+    case let .httpError(code, message, _):
+      return "HTTPError: \(code ?? 0): \(message ?? "Unknown")"
     case let .clientError(error):
       return "ClientError: \(error?.localizedDescription ?? "Unknown")"
     case let .routeError(boxed, _, errorSummary, _):
-      guard let error = boxed.unboxed as? Files.UploadError else { return "Unknown Error" }
+      // TODO: Handle other types of route errors eventually
+      guard let error = boxed.unboxed as? Files.UploadError else { return "Route Error: Unknown" }
       switch error {
       case let .path(writeFailed):
         switch writeFailed.reason {
         case .insufficientSpace:
-          return "Your Dropbox out of storage space."
+          return "Dropbox full. Free some space and try again."
         case .tooManyWriteOperations:
           return "Server busy. Please try again."
         default:
