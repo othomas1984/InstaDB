29c6e9aadc6eb9a5269fd3ac519ffdeedf13f21b
Owen Thomas <owen@owenhthomas.com>
2018-08-22 18:53:33 -0700

    Remove unnecessary parentheses

--- InstaDB/Extensions/CallErrorExtension.swift
+++ InstaDB/Extensions/CallErrorExtension.swift
@@ -12,19 +12,19 @@ extension CallError {
   var errorDescription: String {
     switch self as CallError {
     case let .internalServerError(code, message, requestId):
-      return("InternalServerError[\(requestId ?? "Unknown")]: \(code): \(message ?? "Unknown")")
+      return "InternalServerError[\(requestId ?? "Unknown")]: \(code): \(message ?? "Unknown")"
     case let .badInputError(message, requestId):
-      return("BadInputError[\(requestId ?? "Unknown")]: \(message ?? "Unknown")")
+      return "BadInputError[\(requestId ?? "Unknown")]: \(message ?? "Unknown")"
     case let .authError(authError, userMessage, errorSummary, requestId):
-      return("AuthError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(authError)")
+      return "AuthError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(authError)"
     case let .accessError(accessError, userMessage, errorSummary, requestId):
-      return("AccessError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(accessError)")
+      return "AccessError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(accessError)"
     case let .rateLimitError(rateLimitError, userMessage, errorSummary, requestId):
-      return("RateLimitError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(rateLimitError)")
+      return "RateLimitError[\(requestId ?? "Unknown")]: \(userMessage ?? "Unknown") \(errorSummary ?? "Unknown") \(rateLimitError)"
     case let .httpError(code, message, requestId):
-      return("HTTPError[\(requestId ?? "Unknown")]: \(code ?? 0): \(message ?? "Unknown")")
+      return "HTTPError[\(requestId ?? "Unknown")]: \(code ?? 0): \(message ?? "Unknown")"
     case let .clientError(error):
-      return("ClientError: \(error?.localizedDescription ?? "Unknown")")
+      return "ClientError: \(error?.localizedDescription ?? "Unknown")"
     case let .routeError(boxed, _, errorSummary, _):
       guard let error = boxed.unboxed as? Files.UploadError else { return "Unknown Error" }
       switch error {
