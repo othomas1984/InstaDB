fe9b96e8458de08f673553be2cb73dd96165e46a
Owen Thomas <owen@owenhthomas.com>
2018-08-22 20:21:17 -0700

    Comment

--- InstaDB/Views/ImageCollection/ImageCollectionViewModel.swift
+++ InstaDB/Views/ImageCollection/ImageCollectionViewModel.swift
@@ -18,15 +18,15 @@ class ImageCollectionViewModel {
   let delete: AnyObserver<Image>
 
   init(_ fileService: FileService = FileService()) {
+    // Link observers to publish subjects, which can pass the observed values on as observable sequences
     let reloadImagesSubject = PublishSubject<()>()
     loadImages = reloadImagesSubject.asObserver()
-
     let imageSizeObserverSubject = PublishSubject<Int>()
     imageSizeObserver = imageSizeObserverSubject.asObserver()
-    
     let deleteSubject = PublishSubject<Image>()
     delete = deleteSubject.asObserver()
     
+    // Delete file then call reload images when delete event received
     deleteSubject.subscribe { event in
       guard case let .next(image) = event, let path = image.path else { return }
       fileService.delete(path) { _ in
@@ -34,6 +34,7 @@ class ImageCollectionViewModel {
       }
     }.disposed(by: disposeBag)
 
+    // Create observable which will send events any time any uploading file has a state change
     fileUploadProgress = Observable<[FileService.Path: FileService.UploadState]>.create { observer in
       let handle = fileService.listenForFileUploadChanges { changes in
         changes.forEach { change in
@@ -48,6 +49,7 @@ class ImageCollectionViewModel {
       }
     }
     
+    // Create an observable will fetch an updated file list any time reload images events are sent
     let newImages = reloadImagesSubject.map { _ in
       Observable<[Image]>.create { observer in
         fileService.fetchFileList(path: "") { images, errorDescription in
@@ -62,7 +64,11 @@ class ImageCollectionViewModel {
       }
     }.merge().share()
     
+    // Create an observable which will update the image list any time either new image events are sent,
+    // or the image size change event is sent
     images = Observable.combineLatest(newImages, imageSizeObserverSubject).map { images, _ in images }
+    
+    // Listen for image size toggle changes and update the target pixel size for images on next reload
     imageSizeObserverSubject.subscribe { [unowned self] event in
       guard case let .next(value) = event else { return }
       self.imageSize.onNext(CGFloat((value + 1) * 125))
